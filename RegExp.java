95e116fd4bd07911d3511c8eca9cb42105463859
su ting kuan <128878255+stksax@users.noreply.github.com>
2024-03-06 18:19:12 +0800

    Update RegExp.java

--- RegExp.java
+++ RegExp.java
@@ -4,6 +4,60 @@ import java.io.IOException;
 
 
 public class RegExp {
+
+    public static String test1(String[] args, String word){
+        bool isPalindrome = false;
+        while (args != null) {
+            ;
+            if (args == word){
+                containWord =true;
+                break;
+            }
+        }
+        String ans = containWord ? "Y" : "N";
+    } 
+
+    public static String test2(String[] args, String word){
+        bool containWord = false;
+        while (args != null) {
+            ;
+            if (args == word){
+                containWord =true;
+                break;
+            }
+        }
+        String ans = containWord ? "Y" : "N";
+    } 
+
+    public static String test3(String[] args, String word, int round){
+        int containWord = 0;
+        while (args != null) {
+            ;
+            if (args == word){
+                containWord += 1;
+            }
+        }
+        String ans = containWord > round ? "Y" : "N";
+    } 
+
+    public static String test4(String[] letter){
+        int containLetterA = 0;
+        int containLetterB = 0;
+        while (letter != b && letter != null) {
+            ;
+            if (letter == a){
+                containLetterA += 1;
+            }
+        }
+
+        while (letter != b && letter != null) {
+            ;
+            if (letter == b){
+                containLetterB += 1;
+            }
+        }
+        String ans = containLetterB*2 > containLetterA ? "Y" : "N";
+    } 
     
     public static void main(String[] args) {
         String str1 = args[1];
@@ -11,12 +65,10 @@ public class RegExp {
         int s2Count = Integer.parseInt(args[3]);
 
         boolean check = true;
-        
-        
+           
         int ans3count = 0;
         int len = 0;
         
-
         try {
             BufferedReader reader = new BufferedReader(new FileReader(args[0]));
             String line;
