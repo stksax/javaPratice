4634d639324f16e2f9bd595413977fce17c66dd6
su ting kuan <128878255+stksax@users.noreply.github.com>
2024-03-21 12:07:35 +0800

    n

--- RegExp.java
+++ RegExp.java
@@ -3,94 +3,88 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class RegExp {
-
-    public static String test1(String[] args, String word, int len){
-        boolean isPalindrome = true;
-        String[] record;
-        for (int i = 0; i < len; i++) {
-            if (i < (len / 2)) {
-                record[i] = args[i]; 
-            } else {
-                ; 
-                if (args[i] != record[len - i - 1]) {
-                    isPalindrome = false;
-                    break;
-                }
-            }
-        }
-        String ans = isPalindrome ? "Y" : "N";
-        System.out.println(ans);
-    } 
-
-    public static String test2(String[] args, String word){
-        boolean containWord = false;
-        while (args != null) {
-            ;
-            if (args == word){
-                containWord =true;
-                break;
-            }
-        }
-        String ans = containWord ? "Y" : "N";
-        System.out.println(ans);
-    } 
-
-    public static String test3(String[] args, String word, int round){
-        int containWord = 0;
-        while (args != null) {
-            ;
-            if (args == word){
-                containWord += 1;
-            }
-        }
-        String ans = containWord > round ? "Y" : "N";
-        System.out.println(ans);
-    } 
-
-    public static String test4(String[] letter){
-        int containLetterA = 0;
-        int containLetterB = 0;
-        String a = "a";
-        String b = "b";
-        while (letter != b && letter != null) {
-            ;
-            if (letter == a){
-                containLetterA += 1;
-            }
-        }
-
-        while (letter != b && letter != null) {
-            ;
-            if (letter == b){
-                containLetterB += 1;
-            }
-        }
-        String ans = containLetterB*2 > containLetterA ? "Y" : "N";
-        System.out.println(ans);
-    } 
     
     public static void main(String[] args) {
         String str1 = args[1];
         String str2 = args[2];
-        
+        int s2Count = Integer.parseInt(args[3]);
+
         try {
             BufferedReader reader = new BufferedReader(new FileReader(args[0]));
             String line;
-            int len = 0;
             while ((line = reader.readLine()) != null) {
-                String[] word = line.split(" ");
-                len+=1;
-            }
+                boolean isPalindrome = checkPalindrome(line);
 
-            for (int i = 0; i < len; i++){
-                test2(word[i], str1);
-                test3(word[i], str2, args[3]);
-                test4(word[i]);
+                boolean containStr1 = line.contains(str1) ? true : false;
+
+                boolean containStr2Ntimes = countOccurrences(line, str2) >= s2Count ? true : false;
+
+                boolean containsPattern = checkPattern(line);
+
+                // Print results
+                System.out.print((isPalindrome ? "Y" : "N") + ",");
+                System.out.print((containStr1 ? "Y" : "N") + ",");
+                System.out.print((containStr2Ntimes ? "Y" : "N") + ",");
+                System.out.println((containsPattern ? "Y" : "N"));
             }
-    
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static boolean checkPalindrome(String str) {
+        str = str.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left++) != str.charAt(right--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int countOccurrences(String str, String subStr) {
+        str = str.toLowerCase();
+        subStr = subStr.toLowerCase();
+        int count = 0;
+        int lastIndex = 0;
+        while ((lastIndex = str.indexOf(subStr, lastIndex)) != -1) {
+            count++;
+            lastIndex += subStr.length();
+        }
+        return count;
+    }
+    public static boolean checkPattern(String str) {
+        int aCount = 0;
+        int bCount = 0;
+        boolean ans = false;
+        int switcha = 0;
+        str = str.toLowerCase();
+        int i = 0;
+
+        while (i < str.length()){
+            if (str.charAt(i) == 'a' && switcha == 0){
+                aCount++;
+            }
+            else if (str.charAt(i) == 'b' && switcha == 0){
+                switcha = 1;
+                bCount++;
+            }
+            else if (str.charAt(i) == 'b' && switcha == 1){
+                bCount++;
+            }
+            else if (str.charAt(i) == 'a' && switcha == 1){
+                switcha = 0;   
+                if (bCount == 2 * aCount){
+                    ans = true;
+                }
+                bCount = 0;
+                aCount++;
+            }
+            i++;
+        }
+        return ans;
     }
 }
